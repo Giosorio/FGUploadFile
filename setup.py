65ca73bc79c835f619d5cebd959e2bdf41039af8
Giovanni <giosorio29@gmail.com>
2022-01-16 16:09:36 +0000

    folder name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 
 setup(
-    name='FGUploadFiles',
+    name='FGUploadFile',
     packages=find_packages(include=['FGUploadFile']),
     version='0.1.0',
     description='Class UploadFile that allows to manipulate Fieldglass Upload Files, Spliting the Description from the content and allows you to concatenate them again',
