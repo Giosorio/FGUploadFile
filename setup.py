12c3970c84ceb95bdb528a18e60083188e1d19de
Giovanni <giosorio29@gmail.com>
2022-01-16 16:06:34 +0000

    Folder name changed

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='FGUploadFiles',
-    packages=find_packages(include=['FGUploadFiles']),
+    packages=find_packages(include=['FGUploadFile']),
     version='0.1.0',
     description='Class UploadFile that allows to manipulate Fieldglass Upload Files, Spliting the Description from the content and allows you to concatenate them again',
     author='Giovanni Osorio',
