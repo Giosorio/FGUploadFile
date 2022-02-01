793506ac9ceb01fc8ac2e714d027e72f254fc6f6
Giovanni <giosorio29@gmail.com>
2022-02-01 21:14:40 +0000

    Update setup.py specifying openpyxl version

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
     description='Class UploadFile that allows to manipulate Fieldglass Upload Files, Spliting the Description from the content and allows you to concatenate them again',
     author='Giovanni Osorio',
     licence='MIT',
-    install_requires=['pandas', 'openpyxl'],
+    install_requires=['pandas', 'openpyxl==3.0.9'],
     test_suite='tests',
 )
