85050fb396657c1cfdd02d500a24ad53b85f9922
Giovanni <giosorio29@gmail.com>
2022-02-03 10:18:46 +0000

    Update 0.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 setup(
     name='FGUploadFile',
     packages=find_packages(include=['FGUploadFile']),
-    version='0.4',
+    version='0.5',
     description='Class UploadFile that allows to manipulate Fieldglass Upload Files, Spliting the Description from the content and allows you to concatenate them again',
     author='Giovanni Osorio',
     licence='MIT',
